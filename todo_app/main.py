519bbba2d671e7e0c40eda6f6b18f3c0071789df
DragoTX8 <renzo2539@gmail.com>
2025-11-30 14:26:19 -0600

    Added remove function and complete functionality.

--- todo_app/main.py
+++ todo_app/main.py
@@ -8,6 +8,11 @@ def add_to_list(existing_list: list, new_item, location=None) -> None:
         existing_list[location] = new_item
 
 
+def remove_from_list(existing_list: list, index_to_remove: int):
+    removed_task = existing_list.pop(index_to_remove - 1)
+    return removed_task
+
+
 def display_list_ordered(task_list: list) -> None:
     for index, task in enumerate(task_list):
         print(f"{index + 1}. {task.title()}")
@@ -22,19 +27,45 @@ def main():
     tasks: list = []
 
     while True:
-        user_action = input("Type add, show, edit or exit: ").strip().lower()
+        user_action = input("Type add, complete, show, edit or exit: ").strip().lower()
 
         match user_action:
             case "exit":
                 clear_console()
                 display_list_ordered(tasks)
                 break
+
             case "add":
                 user_task = input("Enter a todo: ").strip().lower()
                 if user_task in tasks:
                     continue
                 else:
                     add_to_list(tasks, user_task)
+
+            case "complete":
+                if len(tasks) == 0:
+                    print("No tasks available for completing.")
+                    continue
+                else:
+                    display_list_ordered(tasks)
+                    to_remove = (
+                        input("Enter the number of the task to be completed: ")
+                        .strip()
+                        .lower()
+                    )
+                try:
+                    to_remove = int(to_remove)
+                except ValueError:
+                    print(f"{to_remove} is not a number.")
+                    continue
+
+                if to_remove > len(tasks):
+                    print(f"There is no task with number {to_remove}")
+                    continue
+                else:
+                    completed = remove_from_list(tasks, to_remove)
+                    print(f"{completed.title()} completed and removed from list.")
+
             case "edit":
                 if len(tasks) == 0:
                     print("No tasks available for editing.")
@@ -64,7 +95,12 @@ def main():
                         add_to_list(tasks, new_task, to_edit - 1)
 
             case "show":
-                display_list_ordered(tasks)
+                if len(tasks) == 0:
+                    print("There are no tasks to show.")
+                    continue
+                else:
+                    display_list_ordered(tasks)
+
             case _:
                 print(f"'{user_action}' is not a valid option.")
 
